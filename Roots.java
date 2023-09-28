1a92fa99248c4af2ae0e520240970bd74865aabc
48-JASHWANTH <113668191+48-JASHWANTH@users.noreply.github.com>
2023-09-28 18:31:21 +0530

    Update Roots.java

--- Roots.java
+++ Roots.java
@@ -14,9 +14,15 @@ class Roots {
     
     if(d>0){
       System.out.println("The roots are real & distinct");
+      double root1 = (-b+(Math.sqrt((b*b)-(4*a*c))))/2*a;
+      double root2 = (-b-(Math.sqrt((b*b)-(4*a*c))))/2*a;
+      System.out.println(root1);
+      System.out.println(root2);
     }
     else if(d==0){
       System.out.println("The roots are real & equal");
+      float root3 = -b/(2*a);
+      System.out.println(root3);
     }
     else{
       System.out.println("The roots are imaginary");
